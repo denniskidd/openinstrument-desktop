257f7f230fef23b36f776f4c8d53a192661d93a6
Dennis Kidd <dennis.kidd@gmail.com>
2025-04-30 19:53:23 -0400

    Switch to notarytool for Apple notarization

--- build/notarize.js
+++ build/notarize.js
@@ -1,15 +1,20 @@
 const { notarize } = require('electron-notarize');
 
-exports.default = async function notarizing(context) {
+exports.default = async function notarizeApp(context) {
   const { electronPlatformName, appOutDir } = context;
+
   if (electronPlatformName !== 'darwin') return;
 
   const appName = context.packager.appInfo.productFilename;
 
+  console.log('🔐 Notarizing with notarytool...');
+
   await notarize({
-    appBundleId: 'edu.jh.openrequest',
+    appBundleId: 'edu.jhu.openrequest',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS
+    appleIdPassword: process.env.APPLE_ID_PASS,
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID, // <- must be added to your GitHub Secrets
   });
 };
