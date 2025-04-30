ac602d5e12aa9eacd07428dfb0c54444758fa7ae
Dennis Kidd <dennis.kidd@gmail.com>
2025-04-30 18:38:12 -0400

    Include notarize.js and entitlements in build folder

---
+++ build/notarize.js
@@ -0,0 +1,15 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  await notarize({
+    appBundleId: 'edu.jh.openrequest',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS
+  });
+};
